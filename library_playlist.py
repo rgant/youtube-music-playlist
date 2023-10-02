cff09b17358c4802eedcbfea72af28cea97fa2ae
J Rob Gant <rgant@alum.wpi.edu>
2023-10-02 08:27:14 -0400

    YouTube sadly limits playlists to 5,000

--- library_playlist.py
+++ library_playlist.py
@@ -7,6 +7,7 @@ Add any songs from the library that are not in the playlist.
 import logging
 import os
 import sys
+import typing
 
 from ytmusicapi import YTMusic
 
@@ -15,11 +16,58 @@ class ApiError(Exception):
     """Wrapper around error responses from the `ytmusicapi`."""
 
 
+class RainbowLogRecord(logging.LogRecord):  # pylint: disable=too-few-public-methods
+    """Add `colorlevelname` field to the LogRecord class for the rainbow formatter."""
+
+    colorlevelname: str
+
+
+class RainbowLogFormatter(logging.Formatter):
+    """Adds a new key to the format string: `%(colorlevelname)s`."""
+
+    # http://kishorelive.com/2011/12/05/printing-colors-in-the-terminal/
+    levelcolors = {
+        'CRITICAL': '\033[4;31mCRITICAL\033[0m',  # Underlined Red Text
+        'ERROR': '\033[1;31mERROR\033[0m',  # Bold Red Text
+        'WARN': '\033[1;33mWARNING\033[0m',  # Bold Yellow Text
+        'WARNING': '\033[1;33mWARNING\033[0m',  # Bold Yellow Text
+        'INFO': '\033[0;32mINFO\033[0m',  # Light Green Text
+        'DEBUG': '\033[0;34mDEBUG\033[0m',  # Light Blue Text
+        'NOTSET': '\033[1:30mNOTSET\033[0m',  # Bold Black Text
+    }
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Adds the new `colorlevelname` to record and then calls the super."""
+        color_rec = typing.cast('RainbowLogRecord', record)
+        color_rec.colorlevelname = self.levelcolors[record.levelname]
+        return super().format(record)
+
+
+def create_handler() -> 'logging.StreamHandler[typing.TextIO]':
+    """Creates a colorful StreamHandler for logging. But only if this is outputting to a terminal"""
+    handler = logging.StreamHandler()
+    # Check that the stream (default is `stderr`) is a terminal, not a pipe or redirect before using
+    # fancy formatter
+    if handler.stream.isatty():
+        frmt = '%(colorlevelname)s:%(module)s:%(funcName)s:%(lineno)d:%(message)s'
+        formatter = RainbowLogFormatter(frmt)
+        handler.setFormatter(formatter)
+    return handler
+
+
+def chunk_set(songs: set[str]) -> typing.Generator[list[str], None, None]:
+    """Batch set of missing song ids for API."""
+    step = 25
+    song_ids = list(songs)
+    for i in range(0, len(song_ids), step):
+        yield song_ids[i:i + step]
+
+
 def everything_playlist(ytmusic: YTMusic) -> str:
     """Finds or creates the 'Everything' Playlist and returns its ID."""
     logger = logging.getLogger(__name__)
     playlists = ytmusic.get_library_playlists()
-    everything_lists = [pl for pl in playlists if pl['title'] != 'Everything']
+    everything_lists = [pl for pl in playlists if pl['title'] == 'Everything 1']
     num_lists = len(everything_lists)
     if num_lists > 1:
         logger.warning('Too many playlists found.')
@@ -39,14 +87,14 @@ def everything_playlist(ytmusic: YTMusic) -> str:
 
 
 def library_songs(ytmusic: YTMusic) -> set[str]:
-    """Fetch every song in my library."""
+    """Fetch every song in my library except 'Radio Edit's."""
     songs = ytmusic.get_library_songs(limit=None)
     return {s['videoId'] for s in songs if 'radio edit' not in s['title'].lower()}
 
 
 def playlist_songs(ytmusic: YTMusic, playlist_id: str) -> set[str]:
     """Fetch every song in the 'Everything' playlist."""
-    playlist = ytmusic.get_playlist(playlist_id)
+    playlist = ytmusic.get_playlist(playlist_id, limit=None)
     return {t['videoId'] for t in playlist['tracks']}
 
 
@@ -54,11 +102,19 @@ def update_playlist(ytmusic: YTMusic) -> None:
     """Update the 'Everything' playlist with any missing songs from the Library."""
     logger = logging.getLogger(__name__)
     all_songs = library_songs(ytmusic)
+    logger.info('All Songs in Library: %s', f'{len(all_songs):,}')
     playlist_id = everything_playlist(ytmusic)
+    logger.debug('Everything Playlist %r', playlist_id)
     existing_songs = playlist_songs(ytmusic, playlist_id)
+    logger.info('Songs in Playlist: %s', f'{len(existing_songs):,}')
+
     missing_songs = all_songs - existing_songs
-    logger.info('Everything playlist is missing %d songs', len(missing_songs))
-    ytmusic.add_playlist_items(playlist_id, list(missing_songs))
+    logger.info('Everything playlist is missing %s songs', f'{len(missing_songs):,}')
+
+    for chunk in chunk_set(missing_songs):
+        logger.info('Adding Songs %d', len(chunk))
+        logger.debug('Song Ids %r', chunk)
+        ytmusic.add_playlist_items(playlist_id, chunk)
 
 
 def main() -> None:
@@ -72,7 +128,7 @@ def main() -> None:
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(handlers=[create_handler()], level=logging.INFO)
     try:
         main()
     except KeyboardInterrupt:
